668e1d5967a4d079015b76fa2507dce13743dd62
Colin Miller <cjmiller@uw.edu>
2015-03-24 17:38:35 -0700

    Working English -> German implementation

--- deutschify.js
+++ deutschify.js
@@ -3,20 +3,23 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log("Deutschify is active!");
   var form = document.getElementById("translate");
+  var switchButton = document.getElementById("switch");
+
   form.addEventListener('submit', translate, false);
+  switchButton.addEventListener('click', switchLanguages, false);
 
   function translate(evt) {
     evt.preventDefault();
     var input = document.getElementById("word").value;
     console.log(input);
 
-    var searchUrl = 'https://glosbe.com/gapi/translate?from=eng&dest=deu&format=json&phrase=' + encodeURIComponent(input.toLowerCase());
+    var from = document.getElementById("from").dataset.lang;
+    var to = document.getElementById("to").dataset.lang;
+    var searchUrl = 'https://glosbe.com/gapi/translate?from=' + from + '&dest=' + to + '&format=json&phrase=' + encodeURIComponent(input.toLowerCase());
     var ajax = new XMLHttpRequest();
     ajax.open('GET', searchUrl);
-    // The Google image search API responds with JSON, so let Chrome parse it.
     ajax.responseType = 'json';
     ajax.onload = function() {
-      // Parse and process the response from Google Image Search.
       var response = ajax.response;
       if (!response || !response.tuc || response.tuc.length === 0) {
         console.log('No response from search!');
@@ -30,5 +33,22 @@ document.addEventListener('DOMContentLoaded', function() {
       console.log('Network error.');
     };
     ajax.send();
+  }
+
+  function switchLanguages() {
+    var fromEle = document.getElementById("from");
+    var toEle = document.getElementById("to");
+
+    if (fromEle.dataset.lang === "eng") {
+      fromEle.dataset.lang = "deu";
+      fromEle.innerHTML = "German";
+      toEle.dataset.lang = "eng"
+      toEle.innerHTML = "English";
+    } else {
+      toEle.dataset.lang = "deu";
+      toEle.innerHTML = "German";
+      fromEle.dataset.lang = "eng"
+      fromEle.innerHTML = "English";
+    }
   }
 });
